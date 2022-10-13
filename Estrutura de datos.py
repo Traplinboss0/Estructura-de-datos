73b26443600f53669756fd44bd17385e782ccce4
traplinboss0 <nmoreno429@misena.edu.co>
2022-10-13 00:43:11 -0500

    Se Sube el archivo con las estructuras correspondientes en un archivo de python

---
+++ Estrutura de datos.py
@@ -0,0 +1,60 @@
+#Metodo append
+lista = [1,2,3,4,5]
+lista.append(6)
+print(lista)
+
+#metodo clear
+lista.clear()
+print(lista)
+
+#metodo extend
+lista1 = [1,2,3]
+lista2 = [4,5,6]
+lista1.extend(lista2)
+print(lista1)
+
+#metodo count
+Universo = ["mundo", "tierra", "universo", "planeta","planeta","mundo","planeta","universo","tierra"]
+print(Universo.count("planeta"))
+
+#metodo index
+Universo = ["mundo", "tierra", "universo", "planeta","planeta","mundo","planeta","universo","tierra"]
+print(Universo.index("universo"))
+
+#metodo insert
+lis = [1,2,3]
+lis.insert(0,0)
+print(lis)
+
+lis1 = [5,10,15,25]
+lis1.insert(-1,20)
+print(lis1)
+
+#metodo pop
+numeros = [10,20,30,40,50]
+print(numeros.pop())
+print(numeros)
+
+#metodo remove
+l = [20,30,30,30,40]
+l.remove(30)
+print(l)
+
+#metodo reverse
+reverso = [10,9,8,7,6,5,4]
+revero.revers()
+print(reverso)
+
+#metodo sort
+ordena = [5,-10,35,0,-65,100]
+ordena.sort()
+print(ordena)
+
+
+
+
+
+
+
+
+
